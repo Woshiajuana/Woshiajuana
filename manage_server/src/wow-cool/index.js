b4ffe7437a925b9dd9a699ad860e8995c3b6146a
czg <czg@jfpal.com>
2018-05-03 10:45:07 +0800

    更新方法

--- manage_server/src/wow-cool/index.js
+++ manage_server/src/wow-cool/index.js
@@ -3,27 +3,26 @@ export default {
 
     /**
      * 查询数组序列
-     * @param  arr      [Array]         需要查询的数组
-     * @param  filter   [Function]      过滤条件
+     * @param  arr      [Array]                         需要查询的数组
+     * @param  filter   [Function, String, Number]      过滤条件
      * */
     findFirstIndexForArr (arr = [], filter = () => {}) {
         if (!this.isArray(arr)) throw Error('arr must is array');
-        if (typeof filter !== 'function') throw Error('filter must is function');
         let index = -1;
         arr.forEach((item, i) => {
-            if(filter(item)) return index = i;
+            if(typeof filter === 'function' && filter(item)) return index = i;
+            if(typeof filter !== 'function' && item === filter) return index = i;
         });
         return index;
     },
 
     /**
      * 查询数组序列
-     * @param  arr      [Array]         需要查询的数组
-     * @param  filter   [Function]      过滤条件
+     * @param  arr      [Array]                         需要查询的数组
+     * @param  filter   [Function, String, Number]      过滤条件
      * */
     findLastIndexForArr (arr = [], filter = () => {}) {
         if (!this.isArray(arr)) throw Error('arr must is array');
-        if (typeof filter !== 'function') throw Error('filter must is function');
         arr = arr.reverse();
         return this.findFirstIndexForArr(arr, filter);
     },
